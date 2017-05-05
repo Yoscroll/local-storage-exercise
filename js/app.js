d74bfc941fefc47284528251617d4a6d93943fa7
Yoscroll <ianhenry.c808@gmail.com>
2017-05-05 11:28:10 -1000

    i didn't mean to fork ms.nat's code :( but i understand lol

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//creates a function that generates a empty object that will be saved to localStorage
+
 (function(){
   var user = {
     id: 0,
@@ -8,39 +8,38 @@
     mobile: "",
     gender: "",
   }
-
-  //creates object that contains methods for localStorage handling
   var handler = {
-    //saves user inputs as one entry to local storage
+    //saves info to local storage
     saveEntry: function (){
       var inputs = document.querySelectorAll(".tcell");
         user.id = inputs[0].value;
         user.name = inputs[1].value;
         user.address = inputs[2].value;
         user.email = inputs[3].value;
-        //converts object into JSON and store in localStorage
         localStorage.setItem("user_" + localStorage.length, JSON.stringify(user));
-        //reloads the page
         location.reload();
     },
 
-    //clears user input fields on the page
+    //clears textboxes
     clearEntry: function(){
       var inputs = document.querySelectorAll(".tcell");
       for(i = 0; i< inputs.length; i++){
         inputs[i].value = "";
       };
     },
-
-    //displays user entries
+    clearAll: function(){
+      localStorage.clear(); 
+      location.reload();
+    },
+    //displays entries
     displayEntry: function(){
       if (localStorage.length > 0){
         var render = "<div>";
         render += "<div id='entry_containter'>Entries:</div>";
         for(i = 0; i < localStorage.length; i++){
-          var key = localStorage.key(i); //gets the key
-          var entry = localStorage.getItem(key); //gets data from key
-          var data = JSON.parse(entry); //parses data back into object
+          var key = localStorage.key(i);
+          var entry = localStorage.getItem(key);
+          var data = JSON.parse(entry);
           render += "<ul>";
           render += "<li>" + data.id + "</li>";
           render += "<li>" + data.name + "</li>";
@@ -54,14 +53,12 @@
     }
   };
 
-  //Save Button Function
   var save = document.getElementById('save');
     save.addEventListener('click', handler.saveEntry);
-
-  //Clear User Input Function
   var clear = document.getElementById('clear');
     clear.addEventListener('click', handler.clearEntry);
-
+  var clearAll = document.getElementById('clear_storage');
+    clearAll.addEventListener('click', handler.clearAll);
   window.onload = function () {
     handler.displayEntry();
   };
